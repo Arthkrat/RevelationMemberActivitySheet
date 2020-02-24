be1a824e426c542e19256f3e4dcbc584ab139758
lialinoleg <lialin@edsson.com>
2020-02-24 16:03:59 +0200

    Added fights info page

--- src/App.vue
+++ src/App.vue
@@ -27,8 +27,9 @@
         <v-list-item>
           <add-new-member></add-new-member>
         </v-list-item>
-        <v-list-item class="pl-1 pr-1">
-          <v-btn to="/addorz" width="100%">Add Orz</v-btn>
+        <v-list-item class="pl-1 pr-1 flex-wrap">
+          <v-btn class="mt-2" to="/addorz" width="100%">Add Orz</v-btn>
+          <v-btn class="mt-2" to="/fillFights" width="100%">Add fights info</v-btn>
         </v-list-item>
       </v-list>
     </v-menu>
